39a0b78125b7ed81e043cb56cc67816d0b181216
Manuel Cheveste <manuelcheveste@gmail.com>
2022-05-09 00:44:34 -0300

    Update task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,6 +30,12 @@ export class TasksService {
     return task;
   }
 
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
   deleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
